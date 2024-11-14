b6bfbd16454d30ebe3ae575dcc2abdc86daf5e52
MikeElnathan <mikeson.anthony@gmail.com>
2024-11-14 14:45:21 +0800

    got the calculator screen working as intended, addition seem to work. but pressing any operation sign simply printing more of it into the top screen.

--- mainScript.js
+++ mainScript.js
@@ -1,8 +1,76 @@
 const mainScreen = document.getElementById("bottomScreen");
 mainScreen.style.cssText = 
 "font-family: calibri, serif; color: black; font-weight: 800; padding-left: 10px; padding-top: 5px; font-size: 32px;";
+const minScreen = document.getElementById("topScreen");
+minScreen.style.cssText = 
+"font-family: calibri, serif; color: black; font-weight: 800; padding-left: 10px; padding-top: 5px; font-size: 12px;";
 
-let num1 = 0, num2 = 0;
+
+let num1 = 0, num2 = 0, answer;
+let operationButtonPushed = false;
+let operation = "";
+
+function displayButton(a)
+{
+    let b = document.getElementById(a);
+    b.style.outline = "2px solid red";
+    setTimeout(() => {
+        b.style.outline = ""
+    }, 200);
+}
+
+function displayManagement(a)
+{
+    let b = parseInt(document.getElementById(a).innerText);
+    
+    if (isNaN(b)) //after "operation" button are pushed.
+    {
+        getNumberInput();
+        //minScreen.innerText += mainScreen.innerText;
+        minToMaxScreen();
+        let d = document.getElementById(a).innerText;
+        minScreen.innerText += d;
+    }else{ 
+        mainScreen.innerText += b;
+    }
+}
+
+function getNumberInput()
+{
+    if (operation == "")
+    {
+        
+        num1 = parseInt(mainScreen.innerText);
+    }
+    else if (operation != ""){
+        num2 = parseInt(mainScreen.innerText);
+    }
+}
+
+function returnANswer(operation)
+{
+    // todo
+    switch(operation){
+        case "add":
+            answer = num1 + num2;
+            //mainScreen.innerText = answer;
+            break;
+    }
+}
+
+function minToMaxScreen()
+{
+    minScreen.innerText += mainScreen.innerText;
+
+    if(answer !== undefined){
+        console.log("mintomax");
+        mainScreen.innerText = answer;
+    }
+    else if(answer === undefined){
+        console.log("to clear");
+        mainScreen.innerText = "";
+    } 
+}
 
 document.querySelector(".operations").addEventListener("click", (a) => 
 {
@@ -14,49 +82,119 @@ document.querySelector(".operations").addEventListener("click", (a) =>
         {
             case "add":
                 // function here
-                mainScreen.innerText = "+"; 
-                const add = document.getElementById("add");
-                add.style.outline = "2px solid red";
-                setTimeout(() => {
-                    add.style.outline = "";
-                }, 500);
+                displayManagement("add");
+                displayButton("add");
+                operation = "add";
+                getNumberInput();
                 break;
             case "minus":
                 // function here
-                mainScreen.innerText = "-"; 
-                const minus = document.getElementById("minus");
-                minus.style.outline = "2px solid red";
-                setTimeout(() => {
-                    minus.style.outline = "";
-                }, 500);
+                displayManagement("minus");
+                displayButton("minus");
+                operation = "minus";
+                getNumberInput();
                 break;
             case "divide":
                 // function here
-                mainScreen.innerText = "/"; 
-                const divide = document.getElementById("divide");
-                divide.style.outline = "2px solid red";
-                setTimeout(() => {
-                    divide.style.outline = "";
-                }, 500);
+                displayManagement("divide"); 
+                displayButton("divide");
+                operation = "divide"
+                getNumberInput();
                 break;
             case "multiply":
                 // function here
-                mainScreen.innerText = "x"; 
-                const multiply = document.getElementById("multiply");
-                multiply.style.outline = "2px solid red";
-                setTimeout(() => {
-                    multiply.style.outline = "";
-                }, 500);
+                displayManagement("multiply");
+                displayButton("multiply");
+                operation = "multiply";
+                getNumberInput();
                 break;
             case "clear":
                 // function here
                 mainScreen.innerText = "";
-                const clear = document.getElementById("clear");
-                clear.style.outline = "2px solid red";
-                setTimeout(() => {
-                    clear.style.outline = "";
-                }, 500);
+                minScreen.innerText = "";
+                num1 = 0;
+                num2 = 0;
+                operation = "";
+                displayButton("clear");
                 break;
         }
     }
 });
+
+document.querySelector("#numbers").addEventListener("click", (a) => 
+    {
+        if(a.target.classList.contains("button"))
+        {
+            const action1 = a.target.getAttribute("id");
+    
+            switch (action1)
+            {
+                case "num1":
+                    // function here
+                    
+                    displayManagement("num1");
+                    displayButton("num1");
+                    break;
+                case "num2":
+                    // function here
+                    
+                    displayManagement("num2"); 
+                    displayButton("num2");
+                    break;
+                case "num3":
+                    // function here
+                    
+                    displayManagement("num3"); 
+                    displayButton("num3");
+                    break;
+                case "num4":
+                    // function here
+                    
+                    displayManagement("num4"); 
+                    displayButton("num4");
+                    break;
+                case "num5":
+                    // function here
+                    
+                    displayManagement("num5");
+                    displayButton("num5");
+                    break;
+                case "num6":
+                    // function here
+                    
+                    displayManagement("num6");
+                    displayButton("num6");
+                    break;
+                case "num7":
+                    // function here
+                    
+                    displayManagement("num7");
+                    displayButton("num7");
+                    break;
+                case "num8":
+                    // function here
+                    displayManagement("num8"); 
+                    displayButton("num8");
+                    break;
+                case "num9":
+                    displayManagement("num9");
+                    displayButton("num9");
+                    break;
+                case "num0":
+                    displayManagement("num0");
+                    displayButton("num0");
+                    break;
+                case "equal":
+                    // function here
+                    getNumberInput();
+                    displayButton("equal");
+                    returnANswer(operation);
+                    minToMaxScreen();
+                    operation = "";
+                    answer = undefined;
+                    break;
+            }
+        }
+    });
+
+
